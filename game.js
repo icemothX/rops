40520a0385cb231dba385cdd7f64d59f093ec8a1
icemothX <icemoth101@gmail.com>
2022-08-09 22:51:17 +0200

    Initialize git with existing files
    Has game functionality and basic styles.
    Currently the game runs for 5 rounds.
    Contains three files:
    game.js
    index.html
    style.css

---
+++ game.js
@@ -0,0 +1,97 @@
+console.log("Hello World!");
+
+let score = 0;
+let roundNo = 1;
+let roundMax = 5;
+
+const rockBttn = document.getElementById("rock");
+const paperBttn = document.getElementById("paper");
+const scissorsBttn = document.getElementById("scissors");
+const outputFld = document.getElementById("game-output");
+const scoreFld = document.getElementById("score");
+const vsFld = document.getElementById("vs");
+const roundNoFld = document.getElementById("round");
+const resetBttn = document.getElementById("reset");
+const overFld = document.getElementById("over");
+
+rockBttn.addEventListener("click", () => handleClick("rock"));
+paperBttn.addEventListener("click", () => handleClick("paper"));
+scissorsBttn.addEventListener("click", () => handleClick("scissors"));  
+resetBttn.addEventListener("click", resetGame);
+
+function getComputerItem() {
+  let x = Math.floor(Math.random()*3);
+  switch (true) {
+    case x===0: 
+      return "rock";
+    case x===1: 
+      return "paper";
+    case x===2:
+      return "scissors";
+  }
+}
+
+function playRound(UserItem, ComputerItem) {
+  switch (true) {
+    case UserItem===ComputerItem:
+      return `It's a draw!`;
+  
+    case UserItem==="rock" && ComputerItem==="paper":
+    case UserItem==="paper" && ComputerItem==="scissors":
+    case UserItem==="scissors" && ComputerItem==="rock":
+      return `You have lost...`;
+      
+    case UserItem==="rock" && ComputerItem==="scissors":
+    case UserItem==="paper" && ComputerItem==="rock":
+    case UserItem==="scissors" && ComputerItem==="paper":
+      incrementScore();
+      return `You won!`;
+        
+    default:
+      return "Something went wrong";
+  }
+}
+
+function playGame(UserItem, lastGame=false) {
+  roundNoFld.textContent = `Round ${roundNo}`
+  let ComputerItem = getComputerItem();
+  vsFld.textContent = `${UserItem} vs. ${ComputerItem}`
+  outputFld.textContent = playRound(UserItem, ComputerItem);
+  scoreFld.textContent = `Your score is ${score}`;
+  if (lastGame) endGame();
+    roundNo += 1;
+}
+
+function handleClick(UserItem) {
+  switch (true) {
+    case (roundNo < roundMax):
+      playGame(UserItem);
+      break;
+    case (roundNo === roundMax):
+      playGame(UserItem, true);
+      break;
+    default:
+      resetGame();
+      playGame(UserItem);
+  }
+}
+
+function incrementScore() {
+  score += 1; //happens inside playRound function
+}
+
+function endGame() {
+  overFld.innerText = `Game over!\n
+    Your total score is ${score}\n
+    Press any button to start over...`;
+}
+
+function resetGame() {
+  roundNo =1;
+  score =0;
+  roundNoFld.textContent="";
+  vsFld.textContent="";
+  outputFld.textContent="";
+  scoreFld.textContent="";
+  overFld.textContent="";
+}
