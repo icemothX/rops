0bd67da6ba520d586f4b5a7122c668078c3a0cfc
icemothX <icemoth101@gmail.com>
2022-08-13 15:56:28 +0200

    Change game logic to play until 5 wins
    Modify outputs accordingly (with formatting);

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 console.log("Hello World!");
 
-let score = 0;
+let userScore = 0;
+let computerScore = 0;
 let roundNo = 1;
-let roundMax = 5;
+let scoreMax = 5;
+let winner = "";
 
 const bttns = document.getElementById("buttons");
 const outputFld = document.getElementById("game-output");
@@ -11,6 +13,7 @@ const vsFld = document.getElementById("vs");
 const roundNoFld = document.getElementById("round");
 const resetBttn = document.getElementById("reset");
 const overFld = document.getElementById("over");
+const pressFld = document.getElementById("press");
 
 buttons.addEventListener("click", event => {
   if (event.target.nodeName == "BUTTON") {
@@ -39,13 +42,14 @@ function playRound(UserItem, ComputerItem) {
     case UserItem==="rock" && ComputerItem==="paper":
     case UserItem==="paper" && ComputerItem==="scissors":
     case UserItem==="scissors" && ComputerItem==="rock":
-      return `You have lost...`;
+      computerScore += 1;
+      return `+1 for computer...`;
       
     case UserItem==="rock" && ComputerItem==="scissors":
     case UserItem==="paper" && ComputerItem==="rock":
     case UserItem==="scissors" && ComputerItem==="paper":
-      incrementScore();
-      return `You win!`;
+      userScore += 1;
+      return `+1 for you!`;
         
     default:
       return "Something went wrong";
@@ -57,43 +61,47 @@ function playGame(UserItem, lastGame=false) {
   let ComputerItem = getComputerItem();
   vsFld.textContent = `${UserItem} vs. ${ComputerItem}`
   outputFld.textContent = playRound(UserItem, ComputerItem);
-  scoreFld.textContent = `Your score is ${score}`;
-  if (lastGame) endGame();
+  scoreFld.textContent = `You: ${userScore}, computer: ${computerScore}`;
+  if (checkWinner()) endGame();
     roundNo += 1;
 }
 
+function checkWinner() {
+  if (computerScore >= 5) {
+    winner="computer";
+    return true;
+  }
+  if (userScore >= 5) {
+    winner="user";
+    return true;
+  }
+  return false;
+}
+
 function handleClick(UserItem) {
-  switch (true) {
-    case (roundNo < roundMax):
-      playGame(UserItem);
-      break;
-    case (roundNo === roundMax):
-      playGame(UserItem, true);
-      break;
-    default:
-      resetGame();
+  if (winner!="") resetGame();
       playGame(UserItem);
   }
-}
 
 function incrementScore() {
   score += 1; //happens inside playRound function
 }
 
 function endGame() {
-  overFld.classList.add("gameOver");
-  overFld.innerText = `Game over!\n
-    Your total score is ${score}\n
-    Press any button to start over...`;
+  overFld.innerText = winner==="user" ? 
+  "You win!" : "You lose!";
+  pressFld.style.visibility="visible";
 }
 
 function resetGame() {
   roundNo =1;
-  score =0;
+  userScore =0;
+  computerScore=0;
+  winner="";
   roundNoFld.textContent="Press a button to start";
   vsFld.textContent="";
   outputFld.textContent="";
   scoreFld.textContent="";
   overFld.textContent="";
-  overFld.classList.remove("gameOver");
+  pressFld.style.visibility="hidden";
 }
